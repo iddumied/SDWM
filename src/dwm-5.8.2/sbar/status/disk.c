84768f96ec75b781ab2c05147d2fc7ee17b40832
root <root@Mbook.localdomain>
2011-08-17 19:54:17 +0200

    finished stats

--- src/dwm-5.8.2/sbar/status/disk.c
+++ src/dwm-5.8.2/sbar/status/disk.c
@@ -7,30 +7,73 @@
 static const char *partitions[]     = { "sda","sda1","sda3", "dm-1","dm-2","dm-3" }; // partitons to show informations of
 static const char *part_names[]     = { "all","boot","Dings","swap","root","home" }; // name of the partitons
 static const char *mount_points[]   = { NULL,"/boot","/media/Dings/",NULL,"/","/home" }; // mountpoints of the partitions 
+#define MAXPARTITIONS 10                    // max number of posible partitions mounted if you mount more it will crash
 
 typedef struct {
   unsigned long long read, write, readtime, writetime, ios_in_process, iostime;
 } DiskActions;
 
 typedef struct {
-  char *name, *realname, *mountpoint;
+  char name[256], realname[256], mountpoint[512];
   Bool mounted, active;
   unsigned long free, total;
   DiskActions last,now,between;
 } Disk;
 
-static Disk disks[20];
+static Disk disks[MAXPARTITIONS];
 
 void setup_disk();
 void get_disk_stat();
-void update_media();
+void update_mounts();
 void update_stats();
 
 int main()
 {
-  setup_disk();
   update_stats();
+  while(1==1){
+  sleep(1);
+  update_stats();
+
+  printf("| write | read | write time | read time | I/Os | I/Os time || write | read | write time | read time | I/Os | I/Os time |\n");
+  
+
+  int i;
+  for(i = 0;i < MAXPARTITIONS;i++){
+  if(!disks[i].active) break;
+
+    printf("%llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu \n",disks[i].between.write,disks[i].between.read,disks[i].between.writetime,disks[i].between.readtime,disks[i].between.ios_in_process,disks[i].between.iostime,disks[i].now.write,disks[i].now.read,disks[i].now.writetime,disks[i].now.readtime,disks[i].now.ios_in_process,disks[i].now.iostime);
+
+  }
 }
+}
+
+void update_mounts()
+{
+  // initializing
+  FILE * fp;
+  char * line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  int i, j;
+  
+  // open /proc/stat
+  fp = fopen("/proc/mounts", "r");
+  if (fp == NULL){
+      printf("\nfailed to read /proc/mounts\n");
+      return;
+  }
+  
+  // reading line by line
+  while ((read = getline(&line, &len, fp)) != -1) {
+
+  }
+
+  if (line) free(line);
+  fclose(fp);
+
+}
+
+
 
 void update_stats()
 {
@@ -40,6 +83,7 @@ void update_stats()
   size_t len = 0;
   ssize_t read;
   int i, j, k, l, m;
+  i = 0;
   
   // open /proc/stat
   fp = fopen("/proc/diskstats", "r");
@@ -52,16 +96,16 @@ void update_stats()
   while ((read = getline(&line, &len, fp)) != -1) {
     disks[i].active = True;
 
-    if(line[16] == " ") k = 2; // = disk
-    else k = 3;
+    if(line[16] == ' ') k = j = 2; // = disk
+    else k = j = 3;
     
     // saveing partition name
-    disks[i].realname[k+1] = "\x00";  
+    disks[i].realname[k+1] = '\x00';  
     for(;k >= 0;k--)
-      disks[i].realname[k] = line[j+k];  
+      disks[i].realname[k] = line[13+k];  
 
-    l = 1;
-    for(j = 13+k+1; j < len; j++){
+    l = 0 ;
+    for(j = 13+j+1; j < len; j++){
       switch(l){
         case 3:
           disks[i].now.read = (atoll(line+j) * 512);
@@ -94,7 +138,7 @@ void update_stats()
       l++;
       
       for(; j < len; j++){
-        if(line[j] == " ") break;
+        if(line[j] == ' ') break;
       }
     }    
     i++;
@@ -103,20 +147,23 @@ void update_stats()
 
   if (line) free(line);
   fclose(fp);
- 
-  disks[i].between.read           = disks[i].now.read           - disks[i].last.read;
-  disks[i].between.write          = disks[i].now.write          - disks[i].last.write;
-  disks[i].between.readtime       = disks[i].now.readtime       - disks[i].last.readtime;
-  disks[i].between.writetime      = disks[i].now.writetime      - disks[i].last.writetime;
-  disks[i].between.ios_in_process = disks[i].now.ios_in_process - disks[i].last.ios_in_process;
-  disks[i].between.iostime        = disks[i].now.iostime        - disks[i].last.iostime;
+  
+  for(j = 0;j < MAXPARTITIONS;j++){
+    if(j == i) break;
+    disks[j].between.read           = (disks[j].now.read           - disks[j].last.read);
+    disks[j].between.write          = (disks[j].now.write          - disks[j].last.write);
+    disks[j].between.readtime       = (disks[j].now.readtime       - disks[j].last.readtime);
+    disks[j].between.writetime      = (disks[j].now.writetime      - disks[j].last.writetime);
+    disks[j].between.ios_in_process = (disks[j].now.ios_in_process - disks[j].last.ios_in_process);
+    disks[j].between.iostime        = (disks[j].now.iostime        - disks[j].last.iostime);
 
-  disks[i].last.read           = disks[i].now.read;
-  disks[i].last.write          = disks[i].now.write;
-  disks[i].last.readtime       = disks[i].now.readtime;
-  disks[i].last.writetime      = disks[i].now.writetime;
-  disks[i].last.ios_in_process = disks[i].now.ios_in_process;
-  disks[i].last.iostime        = disks[i].now.iostime;
+    disks[j].last.read           = disks[j].now.read;
+    disks[j].last.write          = disks[j].now.write;
+    disks[j].last.readtime       = disks[j].now.readtime;
+    disks[j].last.writetime      = disks[j].now.writetime;
+    disks[j].last.ios_in_process = disks[j].now.ios_in_process;
+    disks[j].last.iostime        = disks[j].now.iostime;
+  }
 }
 
 void get_disk_stat()
@@ -145,15 +192,6 @@ void get_disk_stat()
 void setup_disk()
 {
   int i,j,length = LENGTH(partitions);
-
-  for(i = 1; i < length; i++)
-  {
-    disks[i].name = part_names[i];
-    disks[i].realname = partitions[i];
-    disks[i].mountpoint = mount_points[i];
-
-    disks[i].mounted = ((disks[i].mountpoint == NULL) ? False : True);
-  }
 
   get_disk_stat();
 }
